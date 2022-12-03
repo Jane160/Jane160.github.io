1a212682bc15ca88647a31a33fc1faf3e48150d5
Jane160 <119126669+Jane160@users.noreply.github.com>
2022-12-03 11:53:23 -0600

    Hola x2

---
+++ pg_web/main.js
@@ -0,0 +1,13 @@
+//alert("bienvenide a mi pagina");
+let app = document.getElementById('typewriter');
+let typewriter = new Typewriter ( app, {
+    loop: true,
+    delay: 75,
+}
+ );
+//typewriter 
+//.pauseFor(2500)
+//.typeString('Conoci a esta banda gracias a un tiktok en este año (2022) y se convirtio en mi banda favorita')
+//.pauseFor(200)
+//.deleteChars(10)
+//.start();
